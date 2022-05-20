1f889c1cdead3c66449e18d36eb26d4952232d15
Ricardo Gonçalves <ricardo@mytiki.com>
2022-05-20 15:53:10 -0300

    implement command manager slice methods

--- lib/src/command_manager/command.dart
+++ lib/src/command_manager/command.dart
@@ -1,28 +1,61 @@
+import 'package:flutter/foundation.dart';
+
+import 'command_manager_service.dart';
 import 'model/command_status.dart';
 
 abstract class Command{
-  CommandStatus status = CommandStatus.idle;
+  late final CommandManagerService _managerService;
+  CommandStatus _status = CommandStatus.idle;
 
+  CommandStatus get status => _status;
+  set status(CommandStatus value){
+    notify();
+    _status = value;
+  }
+
+  @nonVirtual
+  void addManager(CommandManagerService manager){
+    _managerService = manager;
+  }
+
+  @nonVirtual
   void start(){
     status = CommandStatus.running;
     onStart();
   }
+
+  @nonVirtual
   void pause(){
     status = CommandStatus.paused;
     onPause();
   }
+
+  @nonVirtual
   void resumme(){
     status = CommandStatus.running;
     onResume();
   }
+
+  @nonVirtual
   void stop(){
     status = CommandStatus.idle;
     onStop();
+  }
+
+  @nonVirtual
+  void notify(){
+    _managerService.notify(this);
+  }
+
+  @nonVirtual
+  void finish(){
+    _managerService.finishCommand(this);
+    notify();
   }
 
   Function onStart();
   Function onStop();
   Function onPause();
   Function onResume();
-  Function notifyListeners(status, {Object data});
+
 }
--- lib/src/command_manager/command_manager_service.dart
+++ lib/src/command_manager/command_manager_service.dart
@@ -1,26 +1,99 @@
-import 'command.dart';
+import 'package:sqflite_sqlcipher/sqlite_api.dart';
 
-class CommanManagerService{
+import 'command.dart';
+import 'command_manager_repository.dart';
+import 'model/command_manager_model.dart';
+import 'model/command_manager_queue_status.dart';
+import 'model/command_status.dart';
+
+class CommandManagerService{
+
+  final CommandManagerRepository _repository;
+  CommandManagerModel _model = CommandManagerModel();
+
+  CommandManagerService(Database database) :
+      _repository = CommandManagerRepository(database){
+  }
+  
+  Future<void> init() async{
+    _model = CommandManagerModel.fromMap(await _repository.get());
+  }
 
   void addCommand(Command command){
-    _addListenersToNewCommand(command);
+    command.addManager(this);
+    _model.commandQueue.add(command);
+    runQueue();
   }
 
-  void removeCommand(Command command){}
+  void finishCommand(Command command) {
+    command.stop();
+    _model.commandQueue.remove(command);
+    _model.activeCommands--;
+    runQueue();
+  }
 
-  void stopCommand(Command command){}
+  void stopCommand(Command command){
+    command.stop();
+    _model.activeCommands--;
+    runQueue();
+  }
 
-  void startQueue(){}
+  void runQueue(){
+    if(_queueIsRunning()) return;
+    List<Type> runnningTypes = _getRunningTypes();
+    for(Command command in _model.commandQueue){
+      if(_queueIsFull()) break;
+      if(command.status != CommandStatus.running && 
+          !runnningTypes.contains(command.runtimeType)){
+        command.start();
+        _model.activeCommands++;
+      }
+    }
+  }
 
-  void pauseQueue(){}
+  void notify(Command command) {
+    Type commandType = command.runtimeType;
+    if(!_model.listeners.keys.contains(commandType) ||
+        _model.listeners[commandType] == null) return;
+    List<Function(Command)> listeners = _model.listeners[commandType]!;
+    for(Function(Command) listener in listeners){
+      listener(command);
+    }
+  }
 
-  void resumeQueue(){}
+  void pauseQueue(){
+    for(Command command in _model.commandQueue){
+      if(_queueIsFull()) break;
+      if(command.status == CommandStatus.running){
+        command.pause();
+        _model.activeCommands--;
+      }
+    }
+  }
 
-  void _addListenersToNewCommand(Command command){}
+  void resumeQueue(){
+    for(Command command in _model.commandQueue){
+      if(_queueIsFull()) break;
+      if(command.status == CommandStatus.paused){
+        command.resumme();
+        _model.activeCommands++;
+      }
+    }
+  }
 
   void subscribe(Type T, Function(Object) callback){
-    _addNewListenerToCommands(T, callback);
+    _addNewListenerToCommandType(T, callback);
   }
 
-  void _addNewListenerToCommands(Type T, Function(Object) callback){}
+  void _addNewListenerToCommandType(Type T, Function(Object) callback){}
+
+  bool _queueIsRunning() => _model.status == CommandManagerQueueStatus.running;
+
+  bool _queueIsFull() => _model.activeCommands >= _model.activeLimit;
+
+  List<Type> _getRunningTypes() {
+    return _model.commandQueue.where(
+            (command) => command.status == CommandStatus.running).map(
+              (command) => command.runtimeType).toList();
+  }
 }
